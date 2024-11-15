662f9142e2617c28fcc98a2ff2c3d17f2c65200f
BGamboaCar <mgamboacarrica@udesa.edu.ar>
2024-11-15 11:57:43 -0300

    Hice header con nav, el footer, la lista de recetas en HTML. En JS hice el fetch para la lista de recetas, para que aparezcan

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 let buscadorForm = document.querySelector("#buscador-form");
 let buscadorInput = document.querySelector("#buscador");
+let botonCargar = document.querySelector("#botonCargar")
+let cajaPadreRecetas = document.querySelector(".cajaPadreRecetas")
+
+
+
+let limite = 10;
+
+buscarrecetas(limite)
 
 buscadorForm.addEventListener("submit", function(event) {
     if (buscadorInput.value === "") {
@@ -11,3 +19,42 @@ buscadorForm.addEventListener("submit", function(event) {
         event.preventDefault(); 
     }
 });
+
+
+/* Recetas home fetch*/
+let articleRecetas = document.querySelector(".recetaHome")
+
+
+function buscarrecetas(limite) {
+        
+    fetch(`https://dummyjson.com/recipes?limit=${limite}`)
+    .then(function(response) {
+    return response.json()
+    })
+
+    .then(function(data) {
+        console.log(data);
+
+        let recetas = "";
+
+        for (let i = 0; i < data.recipes.length; i++) {
+            const recetaLista = data.recipes[i];
+
+            recetas +=
+            `<article>
+                <img src="${recetaLista.image}" alt="${recetaLista.name}">
+                <h2>${recetaLista.name}</h2>
+                <p>Dificultad:${recetaLista.difficulty}</p>
+                <a href="./receta.html"></a> 
+            </article>`  
+        }
+        cajaPadreRecetas.innerHTML = recetas;
+    })
+
+    .catch(function(error) {
+    console.log("Error: " + error);
+    })
+};
+
+
+
